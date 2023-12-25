70f1f62dfec65f8fb7a8e471f7dd527479cb139a
V!sal <91835953+Visalraj@users.noreply.github.com>
2023-12-25 13:55:31 +0530

    Initial Commit

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -4,13 +4,7 @@ import 'firebase/compat/auth';
 
 
 const firebaseConfig = {
-  apiKey: "YOUR-API-KEY",
-  authDomain: "netflix-clone-9086d.firebaseapp.com",
-  projectId: "netflix-clone-9086d",
-  storageBucket: "netflix-clone-9086d.appspot.com",
-  messagingSenderId: "937472106112",
-  appId: "YOUR-APP-ID",
-  measurementId: "G-K28T5H1EN8",
+//Your-Firebase-config
 };
 
 export const FireBase = firebase.initializeApp(firebaseConfig);
